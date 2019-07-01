3967fabbcd5c83f4f5de06aceb238659a2ee1e33
Soman <parwanysoman@gmail.com>
2019-07-01 17:42:12 +0200

    made button in html and variables and a function in js

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById('button');
+let key = "21174c6013efcdebbbf876d2fc562a8d";
+
+button.addEventListener('click', getResult());
+
+function getResult() {
+	console.log(button);
+}
+
