4175110b858c5dd672096326bc78e38c7ac04304
Soman <parwanysoman@gmail.com>
2019-07-03 11:05:10 +0200

    It gets the value of my input

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
+const Key = "21174c6013efcdebbbf876d2fc562a8d";
+let input = document.getElementById('inputCity');
 let button = document.getElementById('button');
-let key = "21174c6013efcdebbbf876d2fc562a8d";
 
-button.addEventListener('click', getResult());
+input.addEventListener('keypress', getValueOfInput);
 
-function getResult() {
-	console.log(button);
+function getValueOfInput(e) {
+	console.log(e.target.value);
 }
 
+async function getWeather() {
+    let city = input.value;
+    let api = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=21174c6013efcdebbbf876d2fc562a8d&units=metric`;
+    let response = await axios.get(api);
+ 
+}
+
+
+ getWeather();
+
+//function getWeather() {
+//	{
+///		let city = input.value;
+	//:	let api = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=21174c6013efcdebbbf876d2fc562a8d&units=metric`;
+		
+	//	button.addEventListener('click', async function(e) {
+	//		e.preventDefault();
+	//		let response = await axios.get(api).catch(err => {
+//console.log(response.data.list)
+//		})
+//		})
+	
+//}
+//}
