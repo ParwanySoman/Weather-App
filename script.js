c18fbe27e6415f307669cb18561be0966885448e
Soman <parwanysoman@gmail.com>
2019-07-04 16:54:40 +0200

    added the current day

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const key = "21174c6013efcdebbbf876d2fc562a8d";
 let input = document.getElementById('inputCity');
 
-input.addEventListener("keypress", function (e) {
-    if (e.keyCode == 13 || e.which == 13) {
-        getWeather();
-    }
-});
-
 
 async function getWeather() {
    let city = input.value;
@@ -15,3 +9,23 @@ async function getWeather() {
 
 console.log(response.data.list)
 }
+
+input.addEventListener("keypress", function (e) {
+    if (e.keyCode == 13 || e.which == 13) {
+        getWeather();
+    }
+});
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let day = new Date().getDay();
+
+console.log(day)
